1c3c599c73e1973d87a2ca3ffade4a016b93a0c7
Han <646618065@qq.com>
2020-07-29 15:10:21 +1200

    init

---
+++ src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div id="bg"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MainPage"
+  }
+</script>
+
+<style scoped lang="less">
+#bg {
+
+}
+</style>
